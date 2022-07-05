5608c0253daa24762affeab2c4656bbaa61ef65e
Kartikay Singh <kartikaysingh.business@gmail.com>
2022-07-05 19:52:36 +0530

    Build error correction

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
     <div className="footer-container">
       <p>Copyright ©️2022. All Rights Reserved By boAt</p>
       <p className="  icons">
-        <a href="https://www.linkedin.com/in/kartikay-singh-b684bb218"  target="_blank" >
+        <a href="https://www.linkedin.com/in/kartikay-singh-b684bb218"  target="_blank" rel="noopener noreferrer" >
           <AiFillLinkedin />
         </a>
-        <a href="https://github.com/IamKartikay"  target="_blank" >
+        <a href="https://github.com/IamKartikay"  target="_blank" rel="noopener noreferrer" >
         <AiFillGithub />
         </a>
       </p>
